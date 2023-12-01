8d3d27944df8d62390304e374117415ea366341a
fpidote <127120396+fpidote@users.noreply.github.com>
2023-12-01 18:28:13 +0000

    changes made
    Brakes page- accordion- Added H1 blue font. meta tags for all the pages

--- scripts/rps.js
+++ scripts/rps.js
@@ -72,23 +72,3 @@ const rpsGame = () =>{
 }
 rpsGame();
 
-// Newsletter- Federico
-const newsletter= document.querySelector('.newsletter-modal');
-const newsForm= document.getElementById('news-form');
-
-window.addEventListener('load',() => {
-    setTimeout(() => {
-        newsletter.style.display= 'block';
-    }, 3000); 
-});
-
-newsletter.addEventListener('click', (e) =>{
-    if(e.target == newsletter){
-        newsletter.style.display= 'none';
-    }
-});
-
-newsForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    newsletter.style.display= 'none';
-});

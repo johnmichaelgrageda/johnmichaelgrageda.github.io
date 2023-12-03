4e7147c611c146ecbeacc2af9146246b8c85a1e4
fpidote <127120396+fpidote@users.noreply.github.com>
2023-12-03 16:01:37 +0100

    Changes in the newsletter form

--- scripts/newsletter.js
+++ scripts/newsletter.js
@@ -1,20 +1,47 @@
 // Newsletter- Federico
 const newsletter= document.querySelector('.newsletter-modal');
-const newsForm= document.getElementById('news-form');
+const newsForm= document.querySelector('.news-form');
+const newsClose= document.getElementById('close-x');
+const firstShow= document.querySelector('.first-show');
+const subsMessage= document.querySelector('.subscription-messsage');
+
+const newsForm2= document.querySelector('.news-form-2');
+const firstShow2= document.querySelector('.first-show-2');
+const subsMessage2= document.querySelector('.subscription-messsage-2');
 
 window.addEventListener('load',() => {
     setTimeout(() => {
+       if(newsletter) {
         newsletter.style.display= 'block';
+       }
     }, 3000); 
 });
 
-newsletter.addEventListener('click', (e) =>{
-    if(e.target == newsletter){
-        newsletter.style.display= 'none';
-    }
-});
+if (newsletter) {
+    newsletter.addEventListener('click', (e) =>{
+        if (newsletter && e.target == newsletter){
+            newsletter.style.display= 'none';
+        }
+    });
+}
 
 newsForm.addEventListener('submit', (e)=>{
     e.preventDefault();
-    newsletter.style.display= 'none';
+    firstShow.style.display= 'none';
+    subsMessage.style.display='block';
 });
+
+if  (newsClose) {
+    newsClose.addEventListener('click', (e)=>{
+        e.preventDefault();
+        newsletter.style.display= 'none';
+    });
+}
+
+if (newsForm2) {
+    newsForm2.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        firstShow2.style.display= 'none';
+        subsMessage2.style.display='block';
+    });
+}
